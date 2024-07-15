939d6991c59649030b4440c957ef7ac62f30113e
gencher <36341428+gencher@users.noreply.github.com>
2024-07-15 17:28:24 +0300

    wrap/unwrap UI improve

--- src/pages/Dashboard/widgets/Tokens/components/Unwrap/Unwrap.tsx
+++ src/pages/Dashboard/widgets/Tokens/components/Unwrap/Unwrap.tsx
@@ -39,73 +39,75 @@ export const Unwrap: React.FC<UnwrapProps> = ({ onClose, balance }) => {
   };
 
   return (
-    <div className='flex flex-col p-4'>
-      <h2 className='text-2xl text-black font-bold p-2 text-center'>Unwrap WCNET</h2>
-      <div className='flex justify-center'>
-        <div className='bg-white p-6 rounded-lg shadow-md w-96'>
-          <form onSubmit={handleSubmit}>
-            <div className='flex flex-col mb-4'>
-              <label className='text-black mb-1'>Amount</label>
+    <div className='flex flex-col p-4 text-black'>
+      <h2 className='text-2xl font-bold p-2 text-center'>Unwrap WCNET</h2>
+      <div className='text-sm border border-gray-200 mt-5 rounded-xl p-6'>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-10'>
+            <div className='flex justify-between items-center mb-1 mx-1'>
+              <span className='text-xs'>Amount:</span>
+              <span className='text-xs'>Balance: {convertedBalance.toFixed(4)} WCNET</span>
+            </div>
+            <div className='flex items-center p-3 rounded-xl bg-gray-100'>
               <input
-                type='text'
-                name='amount'
+                type='number'
+                placeholder='Amount'
                 value={formik.values.amount}
                 onChange={formik.handleChange}
+                
+                className='bg-transparent pl-3 text-black flex-grow outline-none no-arrows'
+                style={{ minWidth: '0' }}
                 onBlur={formik.handleBlur}
-                className='border border-gray-300 rounded p-2 w-full text-black'
+                name="amount"
               />
               {formik.touched.amount && formik.errors.amount ? (
                 <div className='text-red-600 text-sm'>{formik.errors.amount}</div>
               ) : null}
-            </div>
-            <div className='flex justify-between items-center mb-4'>
-              <span className='text-gray-600'>
-                Balance: {convertedBalance.toFixed(4)} WCNET
-              </span>
+
               <button
-                type='button'
+                type="button"
+                className='bg-blue-500 text-white text-xs px-3 py-1 rounded-full ml-2'
                 onClick={handleMaxClick}
-                className='bg-blue-500 text-white rounded p-2'
               >
                 MAX
               </button>
             </div>
-            <div className='flex items-center mb-4'>
-              <span className='text-black mr-2'>0</span>
-              <input
-                type='range'
-                min='0'
-                max='100'
-                name='sliderValue'
-                value={formik.values.sliderValue}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className='w-full mx-2'
-              />
-              <span className='text-black ml-2'>100</span>
+          </div>
+          <div className='flex items-center mb-10'>
+            <span className='mr-2'>0%</span>
+            <input
+              type='range'
+              min='0'
+              max='100'
+              name='sliderValue'
+              value={formik.values.sliderValue}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className='w-full mx-2'
+            />
+            <span className='ml-2'>100%</span>
+          </div>
+          <div className='flex justify-between items-center'>
+            <button
+              type='button'
+              onClick={onClose}
+              className='bg-gray-300 rounded-lg p-2 w-1/2 mr-2'
+            >
+              Cancel
+            </button>
+            <button
+              type='submit'
+              className='bg-blue-500 text-white rounded-lg p-2 w-1/2 ml-2'
+            >
+              Wrap
+            </button>
+          </div>
+          {errorMessage && (
+            <div className='text-red-600 text-sm mt-2 text-center'>
+              {errorMessage}
             </div>
-            <div className='flex justify-between items-center mb-4'>
-              <button
-                type='button'
-                onClick={onClose}
-                className='bg-gray-300 text-black rounded p-2 w-1/2 mr-2'
-              >
-                Cancel
-              </button>
-              <button
-                type='submit'
-                className='bg-blue-500 text-white rounded p-2 w-1/2 ml-2'
-              >
-                Unwrap
-              </button>
-            </div>
-            {errorMessage && (
-              <div className='text-red-600 text-sm mt-2 text-center'>
-                {errorMessage}
-              </div>
-            )}
-          </form>
-        </div>
+          )}
+        </form>
       </div>
     </div>
   );
