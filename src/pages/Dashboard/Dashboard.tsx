26b8b50f064332afb47547817937467956ee6279
gencher <gencher@live.com>
2024-07-23 14:56:20 +0300

    fix arg

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -202,62 +202,49 @@ const IssueTokenModal: React.FC<IssueTokenModalProps> = ({
   );
 };
 
-const WIDGETS: WidgetType[] = [
-  {
-    title: 'Tokens',
-    widget: Tokens,
-    description: 'Tokens for the connected account',
-    reference:
-      'https://testnet-api.cyber.network/#/accounts/AccountController_getAccountTokens',
-    button: (
-      <button
-        className='text-xs text-white rounded bg-blue-600 hover:bg-blue-700 px-2 py-1'
-        onClick={() => setIsModalOpen(true)}
-      >
-        Issue Token (Beta)
-      </button>
-    )
-  },
-  {
-    title: 'NFTs',
-    widget: NFTs,
-    description: 'NFTs for the connected account',
-    reference:
-      'https://testnet-api.cyber.network/#/accounts/AccountController_getAccountNfts'
-  },
-  {
-    title: 'Transactions',
-    widget: Transactions,
-    description: 'Transactions list for the connected account',
-    reference:
-      'https://testnet-api.cyber.network/#/accounts/AccountController_getAccountTransactions'
-  }
-];
-
 export const Dashboard = () => {
   useScrollToElement();
 
   const [isModalOpen, setIsModalOpen] = useState(false); // Modal state management
+
+  const WIDGETS: WidgetType[] = [
+    {
+      title: 'Tokens',
+      widget: Tokens,
+      description: 'Tokens for the connected account',
+      reference:
+        'https://testnet-api.cyber.network/#/accounts/AccountController_getAccountTokens',
+      button: (
+        <button
+          className='text-xs text-white rounded bg-blue-600 hover:bg-blue-700 px-2 py-1'
+          onClick={() => setIsModalOpen(true)}
+        >
+          Issue Token (Beta)
+        </button>
+      )
+    },
+    {
+      title: 'NFTs',
+      widget: NFTs,
+      description: 'NFTs for the connected account',
+      reference:
+        'https://testnet-api.cyber.network/#/accounts/AccountController_getAccountNfts'
+    },
+    {
+      title: 'Transactions',
+      widget: Transactions,
+      description: 'Transactions list for the connected account',
+      reference:
+        'https://testnet-api.cyber.network/#/accounts/AccountController_getAccountTransactions'
+    }
+  ];
 
   return (
     <AuthRedirectWrapper>
       <div className='flex flex-col gap-6 max-w-2xl w-full'>
         <Account />
         {WIDGETS.map((element) => (
-          <Widget
-            key={element.title}
-            {...element}
-            button={
-              element.title === 'Tokens' ? (
-                <button
-                  className='text-xs text-white rounded bg-blue-600 hover:bg-blue-700 px-2 py-1'
-                  onClick={() => setIsModalOpen(true)}
-                >
-                  Issue Token (Beta)
-                </button>
-              ) : null
-            }
-          />
+          <Widget key={element.title} {...element} button={element.button} />
         ))}
       </div>
       <IssueTokenModal
@@ -268,7 +255,3 @@ export const Dashboard = () => {
     </AuthRedirectWrapper>
   );
 };
-function setIsModalOpen(arg0: boolean): void {
-  throw new Error('Function not implemented.');
-}
-
