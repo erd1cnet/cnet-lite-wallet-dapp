25dd1a1a4c542e953e23f65a8c7fce60a04c171c
firaterdm <33firaterdem@gmail.com>
2024-08-08 15:29:58 +0300

    Update ImageWithFallback.tsx
    show default svg during loading

--- src/pages/SwapNew/components/ImageWithFallback.tsx
+++ src/pages/SwapNew/components/ImageWithFallback.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DEFAULT_SVG_URL } from 'config';
 
 interface ImageWithFallbackProps {
@@ -9,16 +9,19 @@ interface ImageWithFallbackProps {
 
 const ImageWithFallback: React.FC<ImageWithFallbackProps> = ({ src, alt, className }) => {
   const [imageSrc, setImageSrc] = useState(src);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setImageSrc(src);
+    setLoading(true);
   }, [src]);
 
   return (
     <img
-      src={imageSrc}
+      src={loading ? DEFAULT_SVG_URL : imageSrc}
       alt={alt}
       className={className || 'w-6 h-6 mr-2'}
+      onLoad={() => setLoading(false)}
       onError={() => setImageSrc(DEFAULT_SVG_URL)}
     />
   );
